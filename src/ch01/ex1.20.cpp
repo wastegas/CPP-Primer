fcaefda055612bda54e396426c7cc9223b2b1578
wastegas <wastegas@gmail.com>
2020-11-28 11:47:26 -1000

    exercise 1.20

---
+++ src/ch01/ex1.20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+  Sales_item book;
+  // read ISBN, number of copies sold and sale price
+  std::cin >> book;
+  // write ISBN number of copies sold, total revenue, and avg price
+  std::cout << book << std::endl;
+  return 0;
+}
