8953e28f91fa9ed17467cfe4cf61b7ebdbf8a10d
wastegas <wastegas@gmail.com>
2020-11-28 11:56:42 -1000

    excercise 1.25

---
+++ src/ch01/ex1.25.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+  Sales_item total, item;
+  if (std::cin >> total) {
+    while (std::cin >> item) {
+      if (total.isbn() == item.isbn())
+	total += item;
+      else {
+	std::cout << total << std::endl;
+	total = item;
+      }
+    }
+    std::cout << total << std::endl;
+  } else {
+    std::cerr << "No data received" << std::endl;
+    return -1;
+  }
+  return 0;
+}
