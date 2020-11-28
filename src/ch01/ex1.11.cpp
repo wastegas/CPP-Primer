2d017af002cbb2d3d37ab0ce14810918f51ec4e8
wastegas <wastegas@gmail.com>
2020-11-28 11:30:43 -1000

    excercise 1.11

---
+++ src/ch01/ex1.11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Enter two numbers: " << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  while (v1 <= v2) {
+    std::cout << v1 << std::endl;
+    ++v1;
+  }
+
+  return 0;
+}
