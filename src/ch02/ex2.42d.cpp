79528d8971bc936296da1b2ba906f5336cc696fd
wastegas <wastegas@gmail.com>
2020-12-02 12:13:06 -1000

    excercise 2.42d

---
+++ src/ch02/ex2.42d.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Sales_data.h"
+
+
+int main()
+{
+  Sales_data currItem, item;
+  double price = 0, currPrice = 0;
+  
+  if (std::cin >> currItem.bookNo >> currItem.units_sold >> currPrice) {
+    int cnt = 1;
+    while(std::cin >> item.bookNo >> item.units_sold >> price) {
+      if(currItem.bookNo == item.bookNo)
+	++cnt;
+      else {
+	std::cout << currItem.bookNo << " occurs "
+		  << cnt << " times" << std::endl;
+	currItem.bookNo = item.bookNo;
+	cnt = 1;
+      }
+    }
+    std::cout << currItem.bookNo << " occurs "
+	      << cnt << " times" << std::endl;
+  }
+  return 0;
+}
+	
