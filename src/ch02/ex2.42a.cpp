f15881ff950eca928262759413cf93e9458626e7
wastegas <wastegas@gmail.com>
2020-12-02 12:03:34 -1000

    excercise 2.42a

---
+++ src/ch02/ex2.42a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Sales_data.h"
+
+int main()
+{
+  Sales_data book;
+  double price = 0;
+  double average_price = 0;
+  // read ISBN, number of copies sold and sale price
+  std::cin >> book.bookNo >> book.units_sold >> price;
+  // write BOOKNO number of copies sold, total revenue, and avg price
+  if (book.units_sold > 0) {
+    book.revenue = book.units_sold * price;
+    average_price = book.revenue / book.units_sold;
+    std::cout << book.bookNo << " " <<book.units_sold << " " << book.revenue << " "
+	      << average_price << std::endl;
+    return 0;
+  } else {
+    std::cerr << "(no sales)" << std::endl;
+  }
+
+}
