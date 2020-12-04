b56343be19d73e522a753efb3f398535d82778c1
wastegas <wastegas@gmail.com>
2020-12-03 17:52:21 -1000

    excercise 3.2b

---
+++ src/ch03/ex3.2b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string word;
+  while(cin >> word)
+    cout << word << endl;
+
+  return 0;
+}
