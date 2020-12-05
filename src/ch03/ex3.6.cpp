ea67640848d638d878fcbda4fa75258351e9d9c2
wastegas <wastegas@gmail.com>
2020-12-04 19:05:49 -1000

    exercise 2.6

---
+++ src/ch03/ex3.6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string in;
+  cin >> in;
+  for (decltype(in.size()) index = 0; index < in.size(); index++)
+    in[index] = 'X';
+
+  for (auto &c : in)
+    cout << c;
+
+  cout << endl;
+
+  return 0;
+}
