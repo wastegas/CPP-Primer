7c98f15b80b065bcb35846d1bd06993c6832ec2d
wastegas <wastegas@gmail.com>
2020-12-05 21:50:01 -1000

    fix required output

--- src/ch05/ex5.14.cpp
+++ src/ch05/ex5.14.cpp
@@ -6,20 +6,27 @@ using std::endl;  using std::string;
 
 int main()
 {
-  string currWord, word;
+  string currWord, word, most_repeated;
   int cnt = 1;
+  int most_count = 0;
   if (cin >> currWord) {
     while (cin >> word) {
       if (currWord == word)
 	++cnt;
       else {
-	cout << currWord << " occured " << cnt << " times" << endl;
-	cnt = 1;
-	currWord = word;
+	if (cnt > most_count) {
+	  most_count = cnt;
+	  cnt = 1;
+	  most_repeated = currWord;
+	  currWord = word;
+	}
       }
     }
   }
-  cout << currWord << " occured " << cnt << " times" << endl;
+  if (most_count > 0)
+    cout << most_repeated << " occured " << most_count << " times" << endl;
+  else
+    cout << "no words were repeated" << endl;
       
   return 0;
 }
