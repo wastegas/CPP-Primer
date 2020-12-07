c4b0a23107a38a4b8667ef067044679fb85723b7
wastegas <wastegas@gmail.com>
2020-12-06 16:23:35 -1000

    exercise 6.23

---
+++ src/ch06/ex6.23.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iterator>
+
+using std::cout;
+using std::endl;
+
+
+void print(const int *n)
+{
+  cout << *n << endl;
+}
+
+void print(const int arr[], size_t size)
+{
+  for (size_t i = 0; i != size; i++) {
+    cout << arr[i] << endl;
+  }
+}
+
+int main()
+{
+  int i = 0;
+  int j[2] = {0, 1};
+
+  print(&i);
+  print(j, 2);
+
+  return 0;
+}
