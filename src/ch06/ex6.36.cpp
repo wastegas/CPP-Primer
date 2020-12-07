e60f3408e834fcf8f87ed2b2415cee6927ad46aa
wastegas <wastegas@gmail.com>
2020-12-06 20:08:45 -1000

    exercise 3.36

---
+++ src/ch06/ex6.36.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using std::cout; using std::endl;
+using std::end;
+
+int a[] = {0, 1, 2};
+int (&arrRef())[3]
+{
+  return a;
+}
+
+int main()
+{
+  int *p = arrRef();
+  for (int i = 0; i < 3; i++)
+    cout << *p++ << endl;
+  
+
+  return 0;
+}
