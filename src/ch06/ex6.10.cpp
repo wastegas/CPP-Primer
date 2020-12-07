e6accb641843cb7276daa6431de15dfd8ff4668e
wastegas <wastegas@gmail.com>
2020-12-06 14:03:44 -1000

    exercise 6.10

---
+++ src/ch06/ex6.10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void swap(int *n1, int *n2)
+{
+  int tmp = *(n1);
+  *(n1) = *(n2);
+  *(n2) = tmp;
+}
+
+int main()
+{
+  int n1 = 3;
+  int n2 = 4;
+
+  cout << "before swap n1=" << n1 << " n2=" << n2 << endl;
+  swap(&n1, &n2);
+  cout << "after swap n1=" << n1 << " n2=" << n2 << endl;
+
+  return 0;
+}
+    
