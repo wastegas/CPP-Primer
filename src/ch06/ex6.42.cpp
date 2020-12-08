c215743d3caf792e33b5829c8eed609e241d6b40
wastegas <wastegas@gmail.com>
2020-12-07 21:45:37 -1000

    make func inline

--- src/ch06/ex6.42.cpp
+++ src/ch06/ex6.42.cpp
@@ -3,7 +3,8 @@
 using std::cout;   using std::endl;
 using std::string;
 
-string make_plural(size_t ctr, const string &word, const string& ending = "s")
+// add inline exercise 6.45
+inline string make_plural(size_t ctr, const string &word, const string& ending = "s")
 {
   return (ctr > 1) ? word + ending : word;
 }
