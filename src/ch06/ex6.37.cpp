d0ae5ed27d4045a8772b54f7bc5bbaa82327032a
wastegas <wastegas@gmail.com>
2020-12-07 19:58:48 -1000

    exercise 6.37

---
+++ src/ch06/ex6.37.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+using std::cout; using std::endl;
+using std::end;
+
+int a[] = {0, 1, 2};
+using arr3 = int[3];
+
+int (&arrRef())[3]
+{
+  return a;
+}
+
+arr3& getarr()
+{
+  return a;
+}
+
+auto arr3t() -> int(&)[3]
+{
+  return a;
+}
+
+int main()
+{
+  int *p = arrRef();
+  int *p2 = getarr();
+  int *p3 = arr3t();
+  
+  for (int i = 0; i < 3; i++)
+    cout << *p++ << endl;
+
+  for (int i = 0; i< 3; i++)
+    cout << *p2++ << endl;
+
+  for (int i = 0; i < 3; i++)
+    cout << *p3++ << endl;
+
+  return 0;
+}
