b8609773aa1bdd9e4aa7fbe0e2b8a220cba44b1b
wastegas <wastegas@gmail.com>
2020-12-09 12:46:27 -1000

    exercise 7.15

--- include/person.h
+++ include/person.h
@@ -4,6 +4,7 @@
  * Exercise 7.4: Class creation
  * Exercise 7.5: add operations to return name and address
  * Exercise 7.9: add read and print operations
+ * Exercise 7.15: add constructors
  */
 #ifndef PERSON_H
 #define PERSON_H
@@ -12,6 +13,10 @@
 
 class Person
 {
+  Person() = default;
+  Person(const std::string &n, const std::string &a) :
+    PersonName(n), PersonAddress(a) {}
+  Person(std::istream&);
   std::string name() const { return this->PersonName; }       
   std::string address() const { return this->PersonAddress; } 
   std::string PersonName;
@@ -22,6 +27,11 @@ std::istream &read(std::istream &is, Person &p)
 {
   is >> p.PersonName >> p.PersonAddress;
   return is;
+}
+
+Person::Person(std::istream &is)
+{
+  read(is, *this);
 }
 
 std::ostream &print(std::ostream &os, const Person &p)
