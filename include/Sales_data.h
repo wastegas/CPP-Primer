874acf94a751cd8fe90a575b5e8297e18fa2d848
wastegas <wastegas@gmail.com>
2020-12-09 10:07:55 -1000

    exercise 7.2

--- include/Sales_data.h
+++ include/Sales_data.h
@@ -4,9 +4,18 @@
 #include <string>
 
 struct Sales_data {
+  std::string isbn() const { return this->bookNo; } // exercise 7.2
+  Sales_data& combine(const Sales_data&);           // exercise 7.2
   std::string bookNo;
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
+
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+  units_sold += rhs.units_sold;
+  revenue += rhs.revenue;
+  return *this; // return the object on which the function was called obj.combine()
+}
 
 #endif
