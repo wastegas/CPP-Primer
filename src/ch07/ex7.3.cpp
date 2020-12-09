f8411fd07e9a823d6bbb79baf8a1e5e93157199e
wastegas <wastegas@gmail.com>
2020-12-09 10:11:28 -1000

    exercise 7.3

---
+++ src/ch07/ex7.3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Sales_data.h"
+
+void print(const Sales_data &data)
+{
+  std::cout << data.isbn() << " "
+           <<  data.revenue << " "
+	   <<  data.units_sold << " "
+	   <<  data.revenue / data.units_sold << std::endl;
+  return;
+}
+
+int main()
+{
+  Sales_data currItem, item;
+  double price = 0;
+  
+  if (std::cin >> currItem.bookNo >> currItem.units_sold >> price) {
+    currItem.revenue = currItem.units_sold * price;
+    while(std::cin >> item.bookNo >> item.units_sold >> price) {
+      item.revenue = item.units_sold * price;
+      if(currItem.bookNo == item.bookNo) {
+	currItem.combine(item);
+      }
+      else {
+	print(currItem);
+	currItem.bookNo = item.isbn();
+        currItem.revenue = item.revenue;
+	currItem.units_sold = item.units_sold;
+      }
+    }
+    print(currItem);
+  } else {
+    std::cerr << "(no sales)" << std::endl;
+  }
+  return 0;
+}
+	
