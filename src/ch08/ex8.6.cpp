0b4c2598abc2831bfa1bef7bc7c842b7fd0f83cc
wastegas <wastegas@gmail.com>
2020-12-12 09:59:33 -1000

    fix typo

--- src/ch08/ex8.6.cpp
+++ src/ch08/ex8.6.cpp
@@ -11,9 +11,8 @@ using std::ofstream;
 int main(int argc, char **argv)
 {
   double price = 0;
-  if (argc == 3) {
+  if (argc == 2) {
     ifstream in(argv[1]);
-    ofstream out(argv[2]);
     if (!in.is_open()) {
       cerr << "Error, cannot open file " << argv[1] << endl;
       return -1;
@@ -25,19 +24,17 @@ int main(int argc, char **argv)
 	if(currItem.isbn() == item.isbn()) {
 	  currItem.combine(item);
 	} else {
-	  print(out, currItem) << endl;
+	  print(cout, currItem) << endl;
 	  currItem = item;
 	}
       }
-      print(out, currItem) << endl;
+      print(cout, currItem) << endl;
     } else {
       cerr << "(no data?)" << endl;
       in.close();
-      out.close();
       return -1;
     }
     in.close();
-    out.close();
   } else {
     cout << "Usage: " << argv[0] << " <infile> <outfile> " << endl;
     return -1;
