706a2f9a6b242ffb76bf2f5e8f139b4c5d0add85
wastegas <wastegas@gmail.com>
2020-12-12 10:02:45 -1000

    exercise 8.8

---
+++ src/ch08/ex8.8.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+#include "Sales_data.h"
+
+using std::cout;
+using std::endl; using std::cerr;
+using std::ifstream;
+using std::ofstream;
+
+int main(int argc, char **argv)
+{
+  double price = 0;
+  if (argc == 3) {
+    ifstream in(argv[1]);
+    ofstream out(argv[2], ofstream::app);
+    if (!in.is_open()) {
+      cerr << "Error, cannot open file " << argv[1] << endl;
+      return -1;
+    }
+    Sales_data currItem;
+    if (read(in, currItem)) {
+      Sales_data item;
+      while(read(in, item)) {
+	if(currItem.isbn() == item.isbn()) {
+	  currItem.combine(item);
+	} else {
+	  print(out, currItem) << endl;
+	  currItem = item;
+	}
+      }
+      print(out, currItem) << endl;
+    } else {
+      cerr << "(no data?)" << endl;
+      in.close();
+      out.close();
+      return -1;
+    }
+    in.close();
+    out.close();
+  } else {
+    cout << "Usage: " << argv[0] << " <infile> <outfile> " << endl;
+    return -1;
+  }
+
+
+  return 0;
+}
+	
