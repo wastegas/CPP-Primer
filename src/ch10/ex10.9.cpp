3dfb89fdb1a20d8eac16e5d0c8cf528e82c788b2
wastegas <wastegas@gmail.com>
2020-12-21 20:57:50 -1000

    refactor code to a function

--- src/ch10/ex10.9.cpp
+++ src/ch10/ex10.9.cpp
@@ -6,17 +6,17 @@
 
 using namespace std;
 
-int main()
+void elimDup(vector<string>& ivec)
 {
-  vector<string> ivec{"book", "book", "cat", "dog", "book", "car", "cat", "cat"};
-  for (auto const &i : ivec)
-    cout << i << " ";
-  cout << endl;
-  
   sort(ivec.begin(), ivec.end());
   auto end_unique = unique(ivec.begin(), ivec.end());
   ivec.erase(end_unique, ivec.end());
+}
 
+int main()
+{
+  vector<string> ivec{"book", "book", "cat", "dog", "book", "car", "cat", "cat"};
+  elimDup(ivec);
   for (auto const &i : ivec)
     cout << i << " ";
   cout << endl;
