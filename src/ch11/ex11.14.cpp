d19aeb7d4fe5a48088f738fa7ee04d4f4fd333e6
wastegas <wastegas@gmail.com>
2021-01-02 21:55:37 -1000

    exercise 11.14

---
+++ src/ch11/ex11.14.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  map<string, vector<pair<string, int>>> family;
+  string family_name, children;
+  int age;
+  cout << "Enter lastname: " << endl;
+  while(cin >> family_name) {
+    family[family_name];
+    cout << "Enter childrens names, end with '!' for name and age" << endl;
+    while(cin >> children >> age){
+      if (children == "!")
+	break;
+      family[family_name].push_back(make_pair(children, age));
+    }
+    cin.clear();
+    cin.ignore();
+    cout << "Enter lastname: " << endl;
+  }
+
+  for (const auto &f : family) {
+    cout << f.first << endl;
+    for (const auto &c : f.second)
+      cout << "\t" << c.first << " " << c.second  << endl;
+  }
+
+  return 0;
+}
